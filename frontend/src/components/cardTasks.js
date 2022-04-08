d257e26be142c8977389a39ba2f4cefd0663e5a7
crescar <crescar64@gmail.com>
2022-04-08 17:07:40 -0400

    add page new task

--- frontend/src/components/cardTasks.js
+++ frontend/src/components/cardTasks.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import '../styles/generalStyles.css'
 
-
-const CardTask = ({data})=>{
+const CardTask = ({data, Title, Description, UrlImg, handleGetDatos})=>{
     const defaultImg = 'https://unpaper.com/wp-content/uploads/2019/06/22.-How-to-properly-utilise-the-task-list-to-complete-the-list-of-tasks.png'
     if(data){
         const Datos = data.results
         const taks = Datos.map((dato, index)=>
-        <div className="col s12 m6 l4" key={index}>
+        <div className="col s12 m6 l4 cardEfect" key={index}>
             <div className="card">
                 <div className="card-image">
                 <img src={dato.ImgURL || defaultImg} />
@@ -33,17 +33,36 @@ const CardTask = ({data})=>{
     }else{
         return(
             <>
-                <div className="col s12 m6 l6">
-                    <div className="card">
-                        <div className="card-image">
-                        <img src={defaultImg} />
-                        <span className="card-title cyan-text text-darken-1"></span>
+                <div className='row'>
+                    <div className="col s12 m6 l6 cardEfect">
+                        <div className="card">
+                            <div className="card-image">
+                            <img src={defaultImg} />
+                            <span className="card-title cyan-text text-darken-1"></span>
+                            </div>
+                            <div className="card-content center-align">
+                            <p></p>
+                            </div>
                         </div>
-                        <div className="card-content center-align">
-                        <p></p>
-                        </div>
+                    </div> 
+                    <div className="col s12 m6 l6 Fondo">
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <input id="Title" type="text" className="validate"  name="Title"/>
+                                    <label htmlFor="Title" >Title</label>
+                                </div>
+                                <div className="input-field col s12">
+                                    <input id="Img" type="text" className="validate" name="UrlImg"/>
+                                    <label htmlFor="Img">Link Image</label>
+                                </div>
+                                <div class="input-field col s12">
+                                    <textarea id="Description" class="materialize-textarea" name='Description'></textarea>
+                                    <label htmlFor="Description">Description</label>
+                                </div>
+                                <a className="waves-effect waves-light btn-large" >Save</a>
+                            </div>
                     </div>
-                </div> 
+                </div>
             </>
         )
     }  
--- frontend/src/page/newTask.js
+++ frontend/src/page/newTask.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import NavBar from "../components/navbar";
 import CardTask from "../components/cardTasks";
 
 const NewTask = ()=>{
+
+    const [Title, setTitle] = useState('')
+    const [Description, setDescription] = useState('')
+    const [UrlImg, setUrlImg] = useState('')
+    
+    const handleGetDatos = (e)=>{
+        if(e.target.name === 'Title'){
+            setTitle(e.target.value)
+            console.log(Title)
+        }
+        if(e.target.name === 'Description'){
+            setDescription(e.target.value)
+            console.log(Description)
+        }
+        if(e.target.name === 'UrlImg'){
+            setUrlImg(e.target.value)
+            console.log(UrlImg)
+        }
+    }
+
     return(
        <>
         <div className="container">
             <NavBar/>
             <div className="row">
-                <CardTask/>
+                <CardTask
+                Title ={Title}
+                Description = {Description}
+                UrlImg = {UrlImg}
+                handleGetDatos = {handleGetDatos}
+                />
             </div>
         </div>
        </> 
